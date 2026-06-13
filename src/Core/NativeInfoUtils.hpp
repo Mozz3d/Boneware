2cf7dfe11d6d356bc277b3b0281d1459513052cb
Mozz3d <163353426+Mozz3d@users.noreply.github.com>
2026-06-12 23:23:20 -0400

    Update NativeInfoUtils

--- src/Core/NativeInfoUtils.hpp
+++ src/Core/NativeInfoUtils.hpp
@@ -5,7 +5,33 @@
 #include <Core/Plugin.hpp>
 
 template<typename T>
+struct NativeInfoBase {
+    using OwnerType = T;
+};
+
+template<typename T>
 struct NativeInfo;
+
+template<typename T>
+struct NativeInfo : std::conditional_t<
+    std::derived_from<T, NativeInfoBase<T>>,
+    T,
+    NativeInfoBase<T>
+> 
+{};
+
+template<typename TEx, typename TNative>
+struct NativeExpansionBase {
+    using ExType = TEx;
+    using NativeType = TNative;
+};
+
+template<typename TExt, typename TNative>
+struct NativeExpansion;
+
+namespace Natives {
+    using OwnerType = void;
+}
 
 namespace Detail
 {
@@ -18,20 +44,6 @@ namespace Detail
     }
 
     template<typename T>
-    using RawType = std::remove_pointer_t<std::remove_cvref_t<T>>;
-
-    template<typename T>
-    struct NativeInfoBase {
-        using OwnerType = T;
-    };
-
-    template<typename TEx, typename TNative>
-    struct NativeExpansionBase {
-        using ExType = TEx;
-        using NativeType = TNative;
-    };
-
-    template<typename T>
     requires std::derived_from<T, NativeInfoBase<T>>
     NativeInfo<T> NativeInfoFrom(T*);
 
@@ -39,123 +51,187 @@ namespace Detail
     requires std::derived_from<T, NativeInfoBase<T>>
     NativeInfo<T> NativeInfoFrom(const T*);
 
-    // Determine whether ABI will use RVO for this type.
+    // Determine whether ABI will use return storage for this type.
     template<typename T>
-    concept WillRVO = !std::is_void_v<T> && (sizeof(T) > 8 || !std::is_trivially_copyable_v<T>) && !std::is_floating_point_v<T>;
+    concept WillSRet = !std::is_void_v<T> && !std::is_floating_point_v<T> &&
+        (!std::has_single_bit(sizeof(T)) || sizeof(T) > 8 || !std::is_trivially_copyable_v<T>);
 
-    template<typename TOwner, typename TRet, typename... TArgs>
-    struct NativeMemberFuncBase
-    {
-        static constexpr bool HasRVO = WillRVO<TRet>;
-
-        using RetType = TRet;
-
-        using OwnerType = TOwner;
-
-        using ArgsTuple = std::tuple<TArgs...>;
-        template<size_t I>
-        using ArgType = std::tuple_element<I, ArgsTuple>::type;
-
-        // MSVC's pointer to member is a non-standard fat pointer that -given circumstances-
-        // are error prone and difficult to manipulate. So instead we just fake it.
-        // This however means we need to account for RVO manually because the compiler may
-        // fuck up argument registers otherwise.
-        using PtrType = std::conditional_t<
-            HasRVO,
-            void(*)(TOwner*, TRet*, TArgs...),
-            TRet(*)(TOwner*, TArgs...)
-        >;
-
-    protected:
-        template<typename... UArgs>
-        inline static RetType Invoke(PtrType func, OwnerType* aInstance, UArgs&&... aArgs)
-        {
-            if constexpr (HasRVO) {
-                // FIXME
-                // this is bad because it assume RetType is default constructible (it may not be)
-                RetType __return_storage__;
-                func(aInstance, &__return_storage__, std::forward<UArgs>(aArgs)...);
-                return std::move(__return_storage__);
-            }
-            else
-                return func(aInstance, std::forward<UArgs>(aArgs)...);
-        }
-    };
-
-    template<typename TImpl, typename TOwner, typename TFunc>
-    struct NativeMemberFunc;
-
-    template<typename TImpl, typename TOwner, typename TRet, typename... TArgs>
-    struct NativeMemberFunc<TImpl, TOwner, TRet(TArgs...)> : NativeMemberFuncBase<TOwner, TRet, TArgs...> 
-    {
-        using Base = NativeMemberFuncBase<TOwner, TRet, TArgs...>;
-        using typename Base::RetType;
-        using typename Base::OwnerType;
-        using typename Base::PtrType;
-        using typename Base::ArgsTuple;
-    protected:
-        using Base::Invoke;
-    public:
-        template<typename... UArgs>
-        NativeMemberFunc(UArgs&&... aArgs) : boundArgs(std::forward<UArgs>(aArgs)...) {}
-
-        template<typename... UArgs>
-        inline static RetType Call(OwnerType* aInstance, UArgs&&... aArgs)
-        {
-            return Invoke(TImpl::Get(aInstance), aInstance, std::forward<UArgs>(aArgs)...);
-        }
-
-        inline RetType BoundCall(OwnerType* aInstance) &&
-        {
-            return std::apply([aInstance](auto&&... aArgs) {
-                return Call(aInstance, std::forward<decltype(aArgs)>(aArgs)...);
-                }, std::move(boundArgs));
-        }
-
-        ArgsTuple boundArgs;
-    };
-
-    template<typename TImpl, typename TOwner, typename TRet, typename... TArgs>
-    struct NativeMemberFunc<TImpl, TOwner, TRet(TArgs...) const> : NativeMemberFuncBase<const TOwner, TRet, TArgs...> 
-    {
-        using Base = NativeMemberFuncBase<const TOwner, TRet, TArgs...>;
-        using typename Base::RetType;
-        using typename Base::OwnerType;
-        using typename Base::PtrType;
-        using typename Base::ArgsTuple;
-    protected:
-        using Base::Invoke;
-    public:
-        template<typename... UArgs>
-        NativeMemberFunc(UArgs&&... aArgs) : boundArgs(std::forward<UArgs>(aArgs)...) {}
-
-        template<typename... UArgs>
-        inline static RetType Call(OwnerType* aInstance, UArgs&&... aArgs)
-        {
-            return Invoke(TImpl::Get(aInstance), aInstance, std::forward<UArgs>(aArgs)...);
-        }
-
-        inline RetType BoundCall(OwnerType* aInstance)&&
-        {
-            return std::apply([aInstance](auto&&... aArgs) {
-                return Call(aInstance, std::forward<decltype(aArgs)>(aArgs)...);
-                }, std::move(boundArgs));
-        }
-
-        ArgsTuple boundArgs;
-    };
+    //template <typename = void>
+    //concept HasOwner = requires {
+    //    typename OwnerType;
+    //};
 }
 
-template<typename T>
-struct NativeInfo : std::conditional_t<
-    std::derived_from<T, Detail::NativeInfoBase<T>>,
-    T,
-    Detail::NativeInfoBase<T>
-> {
+template<typename TOwner, typename TRet, typename... TArgs>
+struct NativeFuncBase
+{
+    static constexpr bool HasSRet = Detail::WillSRet<TRet>;
+
+    using RetType = TRet;
+
+    using ArgsTuple = std::tuple<TArgs...>;
+    template<size_t I>
+    using ArgType = std::tuple_element<I, ArgsTuple>::type;
+
+    // MSVC's pointer to member is a non-standard fat pointer that -given circumstances-
+    // are error prone and difficult to manipulate. So instead we just fake it.
+    // This however means we need to account for sret manually because the compiler may
+    // fuck up argument registers otherwise.
+    using PtrType =
+        std::conditional_t<!std::is_void_v<TOwner>,
+        std::conditional_t<HasSRet,
+        void(*)(TOwner*, TRet*, TArgs...),
+        TRet(*)(TOwner*, TArgs...)>,
+        std::conditional_t<HasSRet,
+        void(*)(TRet*, TArgs...),
+        TRet(*)(TArgs...)>>;
 };
 
-template<typename TExt, typename TNative>
-struct NativeExpansion;
+template<typename TImpl, typename TOwner, typename TFunc>
+struct NativeFunc;
+
+template<typename TImpl, typename TOwner, typename TRet, typename... TArgs>
+struct NativeFunc<TImpl, TOwner, TRet(TArgs...)> : NativeFuncBase<void, TRet, TArgs...>
+{
+    using Base = NativeFuncBase<void, TRet, TArgs...>;
+    using Base::HasSRet;
+    using OwnerType = TOwner;
+    using typename Base::RetType;
+    using typename Base::ArgsTuple;
+    using typename Base::PtrType;
+    template<size_t I>
+    using ArgType = typename Base::template ArgType<I>;
+
+protected:
+    template<typename... UArgs>
+    inline static RetType Invoke(PtrType func, UArgs&&... aArgs)
+    {
+        if constexpr (HasSRet) {
+            // FIXME
+            // this is bad because it assumes RetType is default constructible (it may not be)
+            // it also assumes RetType is trivially destructible, pray there's no situation where 
+            // cleanup is critical, lest this leak
+            RetType __return_storage__;
+            func(&__return_storage__, std::forward<UArgs>(aArgs)...);
+            return __return_storage__;
+        }
+        else
+            return func(std::forward<UArgs>(aArgs)...);
+    }
+public:
+    template<typename... UArgs>
+    NativeFunc(UArgs&&... aArgs) : boundArgs(std::forward<UArgs>(aArgs)...) {}
+
+    template<typename... UArgs>
+    inline static RetType Call(UArgs&&... aArgs)
+    {
+        return Invoke(TImpl::Get(), std::forward<UArgs>(aArgs)...);
+    }
+
+    inline RetType BoundCall(OwnerType* _ = nullptr)&&
+    {
+        return std::apply([](auto&&... aArgs) {
+            return Call(std::forward<decltype(aArgs)>(aArgs)...);
+            }, std::move(boundArgs));
+    }
+
+    ArgsTuple boundArgs;
+};
+
+template<typename TOwner, typename TRet, typename... TArgs>
+struct NativeMemberFuncBase : NativeFuncBase<TOwner, TRet, TArgs...>
+{
+    using Base = NativeFuncBase<TOwner, TRet, TArgs...>;
+    using Base::HasSRet;
+    using OwnerType = TOwner;
+    using typename Base::RetType;
+    using typename Base::ArgsTuple;
+    using typename Base::PtrType;
+    template<size_t I>
+    using ArgType = typename Base::template ArgType<I>;
+protected:
+    template<typename... UArgs>
+    inline static RetType Invoke(PtrType func, OwnerType* aInstance, UArgs&&... aArgs)
+    {
+        if constexpr (HasSRet) {
+            RetType __return_storage__;
+            func(aInstance, &__return_storage__, std::forward<UArgs>(aArgs)...);
+            return __return_storage__;
+        }
+        else
+            return func(aInstance, std::forward<UArgs>(aArgs)...);
+    }
+};
+
+template<typename TImpl, typename TOwner, typename TFunc>
+struct NativeMemberFunc;
+
+template<typename TImpl, typename TOwner, typename TRet, typename... TArgs>
+struct NativeMemberFunc<TImpl, TOwner, TRet(TArgs...)> : NativeMemberFuncBase<TOwner, TRet, TArgs...>
+{
+    using Base = NativeMemberFuncBase<TOwner, TRet, TArgs...>;
+    using Base::HasSRet;
+    using typename Base::OwnerType;
+    using typename Base::RetType;
+    using typename Base::ArgsTuple;
+    using typename Base::PtrType;
+    template<size_t I>
+    using ArgType = typename Base::template ArgType<I>;
+protected:
+    using Base::Invoke;
+public:
+    template<typename... UArgs>
+    NativeMemberFunc(UArgs&&... aArgs) : boundArgs(std::forward<UArgs>(aArgs)...) {}
+
+    template<typename... UArgs>
+    inline static RetType Call(OwnerType* aInstance, UArgs&&... aArgs)
+    {
+        return Invoke(TImpl::Get(aInstance), aInstance, std::forward<UArgs>(aArgs)...);
+    }
+
+    inline RetType BoundCall(OwnerType* aInstance)&&
+    {
+        return std::apply([aInstance](auto&&... aArgs) {
+            return Call(aInstance, std::forward<decltype(aArgs)>(aArgs)...);
+            }, std::move(boundArgs));
+    }
+
+    ArgsTuple boundArgs;
+};
+
+// const member function
+template<typename TImpl, typename TOwner, typename TRet, typename... TArgs>
+struct NativeMemberFunc<TImpl, TOwner, TRet(TArgs...) const> : NativeMemberFuncBase<const TOwner, TRet, TArgs...>
+{
+    using Base = NativeMemberFuncBase<const TOwner, TRet, TArgs...>;
+    using Base::HasSRet;
+    using typename Base::OwnerType;
+    using typename Base::RetType;
+    using typename Base::ArgsTuple;
+    using typename Base::PtrType;
+    template<size_t I>
+    using ArgType = typename Base::template ArgType<I>;
+protected:
+    using Base::Invoke;
+public:
+    template<typename... UArgs>
+    NativeMemberFunc(UArgs&&... aArgs) : boundArgs(std::forward<UArgs>(aArgs)...) {}
+
+    template<typename... UArgs>
+    inline static RetType Call(OwnerType* aInstance, UArgs&&... aArgs)
+    {
+        return Invoke(TImpl::Get(aInstance), aInstance, std::forward<UArgs>(aArgs)...);
+    }
+
+    inline RetType BoundCall(OwnerType* aInstance)&&
+    {
+        return std::apply([aInstance](auto&&... aArgs) {
+            return Call(aInstance, std::forward<decltype(aArgs)>(aArgs)...);
+            }, std::move(boundArgs));
+    }
+
+    ArgsTuple boundArgs;
+};
 
 
 #define NATIVE_INFO(_owner, ...) \
@@ -165,13 +241,59 @@ struct NativeExpansion;
         NativeInfo<_owner> NativeInfoFrom(const _owner*); \
     } \
     template<> struct NativeInfo<_owner> \
-        : __VA_OPT__(NativeInfo<__VA_ARGS__>,) Detail::NativeInfoBase<_owner>             
+        : __VA_OPT__(NativeInfo<__VA_ARGS__>,) NativeInfoBase<_owner>             
 
 // maybe shouldnt use this
-#define NATIVE_TYPE(_type_def) _type_def : Detail::NativeInfoBase<_type_def>
+#define NATIVE_TYPE(_type_def) _type_def : NativeInfoBase<_type_def>
+
+#define NATIVE_FN(_ret, _name, _params, _hash) \
+    namespace NativeGlobals { \
+        static_assert(std::is_void_v<OwnerType>, \
+        "NATIVE_FN is for globals. Use NATIVE_STATIC_FN for classes instead"); \
+        struct _name : NativeFunc<_name, void, _ret _params> { \
+            using NativeFunc::NativeFunc; \
+            static constexpr const char* Name = #_name; \
+            static constexpr uint32_t Hash = _hash; \
+            inline static const uintptr_t Address{ RED4ext::UniversalRelocBase::Resolve(Hash) }; \
+            inline static bool HasHook = false; \
+            inline static PtrType Original = nullptr; \
+            static PtrType Get() { return reinterpret_cast<PtrType>(Address); } \
+            template<typename... UArgs> \
+            inline static RetType CallOriginal(UArgs&&... aArgs) { \
+                return Invoke(Original, std::forward<UArgs>(aArgs)...); \
+            } \
+            inline RetType BoundCallOriginal() && { \
+                return std::apply([](auto&&... aArgs) { \
+                    return CallOriginal(std::forward<decltype(aArgs)>(aArgs)...); \
+                }, std::move(boundArgs)); \
+            } \
+        } \
+    }
+
+
+#define NATIVE_STATIC_FN(_ret, _name, _params, _hash) \
+    struct _name : NativeFunc<_name, OwnerType, _ret _params> { \
+        using NativeFunc::NativeFunc; \
+        static constexpr const char* Name = #_name; \
+        static constexpr uint32_t Hash = _hash; \
+        inline static const uintptr_t Address{ RED4ext::UniversalRelocBase::Resolve(Hash) }; \
+        inline static bool HasHook = false; \
+        inline static PtrType Original = nullptr; \
+        static PtrType Get() { return reinterpret_cast<PtrType>(Address); } \
+        template<typename... UArgs> \
+        inline static RetType CallOriginal(UArgs&&... aArgs) { \
+            return Invoke(Original, std::forward<UArgs>(aArgs)...); \
+        } \
+        inline RetType BoundCallOriginal() && { \
+        return std::apply([](auto&&... aArgs) { \
+            return CallOriginal(std::forward<decltype(aArgs)>(aArgs)...); \
+            }, std::move(boundArgs)); \
+        } \
+    }
+
 
 #define NATIVE_MEMBER_FN(_ret, _name, _params, _hash) \
-    struct _name : Detail::NativeMemberFunc<_name, OwnerType, _ret _params> { \
+    struct _name : NativeMemberFunc<_name, OwnerType, _ret _params> { \
         using NativeMemberFunc::NativeMemberFunc; \
         static constexpr const char* Name = #_name; \
         static constexpr uint32_t Hash = _hash; \
@@ -191,7 +313,7 @@ struct NativeExpansion;
     }
 
 #define NATIVE_VIRTUAL_FN(_ret, _name, _params, _offset) \
-    struct _name : Detail::NativeMemberFunc<_name, OwnerType, _ret _params> { \
+    struct _name : NativeMemberFunc<_name, OwnerType, _ret _params> { \
         using NativeMemberFunc::NativeMemberFunc; \
         static constexpr const char* Name = #_name; \
         static constexpr uint32_t Offset = _offset; \
@@ -221,7 +343,7 @@ struct NativeExpansion;
 #define NATIVE_EXPAND(_native_type, _extension) \
     template<> struct NativeInfo<_extension> : NativeInfo<_native_type> {}; \
     template<> struct NativeExpansion<_extension, _native_type> \
-        : Detail::NativeExpansionBase<_extension, _native_type>
+        : NativeExpansionBase<_extension, _native_type>
 
 #define NATIVE_DETOUR_MEMBER_FN(_name, _detour) \
     inline static const bool _name##_hook_registered = []() { \
@@ -264,13 +386,22 @@ using NativeMidFuncContext = MidHooking::MidContext;
 
 
 
-
 #define NATIVE_GET(_instance, _field_name) \
     decltype(::Detail::NativeInfoFrom(::Detail::AsTypePtr(_instance)))::_field_name::Get(::Detail::AsTypePtr(_instance))
 
 // ctor used for pretty semantics e.g. NATIVE_CALL( instance,Method(args) )
-#define NATIVE_CALL(_instance, _func_ctor) \
+#define NATIVE_FREE_CALL(_qualified_func_ctor) \
+    ::NativeGlobals::_qualified_func_ctor.BoundCall()
+
+#define NATIVE_MEMBER_CALL(_instance, _func_ctor) \
     decltype(::Detail::NativeInfoFrom(::Detail::AsTypePtr(_instance)))::_func_ctor.BoundCall(::Detail::AsTypePtr(_instance))
+
+#define NATIVE_STATIC_CALL(_owner, _func_ctor) \
+    ::NativeInfo<_owner>::_func_ctor.BoundCall()
+
+#define NATIVE_CALL_SELECT(_1, _2, _macro, ...) _macro
+#define NATIVE_CALL(...) \
+    NATIVE_CALL_SELECT(__VA_ARGS__, NATIVE_MEMBER_CALL, NATIVE_FREE_CALL)(__VA_ARGS__)
 
 // for use with detours
 #define NATIVE_CALL_ORIGINAL(_instance, _func_ctor) \
