06b0c35fb5a2d84159a30705b9c1f041817c4e70
Mozz3d <163353426+Mozz3d@users.noreply.github.com>
2026-07-07 12:55:24 -0400

    Update math lib

--- src/Lib/Math.hpp
+++ src/Lib/Math.hpp
@@ -5,3 +5,8 @@ inline RED4ext::Quaternion& operator*=(RED4ext::Quaternion& lhs, const RED4ext::
     lhs = lhs * rhs;
     return lhs;
 }
+
+inline RED4ext::QsTransform QsIdentity()
+{
+    return { {}, {}, {1.f, 1.f, 1.f, 1.f} };
+}
